30abb76796954979daccf653263489d489d94030
Joel Henry <joel.henry.023@gmail.com>
2022-02-18 14:24:30 -0500

    Config build

--- functions/utils/firebase/firebaseConfig.js
+++ functions/utils/firebase/firebaseConfig.js
@@ -22,11 +22,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
-// const serviceAccount = require("../../../config.json");
-const { applicationDefault } = require("firebase-admin/app");
+const serviceAccount = require("../../../config.json");
+// const { applicationDefault } = require("firebase-admin/app");
+
 firebase.initializeApp({
-  // credential: firebase.credential.cert(serviceAccount)
-  credential: applicationDefault()
+  credential: firebase.credential.cert(serviceAccount)
+  // credential: applicationDefault()
 })
 
 
