b8b203f562cdce3a39e03aa7b464f49f98adc895
Joel Henry <joel.henry.023@gmail.com>
2022-02-20 07:49:13 -0500

    Admin SDK fix

--- functions/utils/firebase/firebaseConfig.js
+++ functions/utils/firebase/firebaseConfig.js
@@ -21,25 +21,15 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 
-// const serviceAccount = {
-//   type: process.env.TYPE,
-//   project_id: process.env.PROJECT_ID,
-//   private_key_id: process.env.PRIVATE_ID,
-//   private_key: process.env.PRIVATE_KEY,
-//   client_email: process.env.CLIENT_EMAIL,
-//   client_id: process.env.CLIENT_EMAIL,
-//   auth_uri: process.env.AUTH_URI,
-//   token_uri: process.env.TOKEN_URL,
-//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_URL,
-//   client_x509_cert_url: process.env.CLIENT_CERT_URL
-// }
 
-// console.log(serviceAccount)
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
+// const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
 // const { applicationDefault } = require("firebase-admin/app");
 
+
+
+
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount)
+  credential: refreshToken(process.env.REFRESH_TOKEN)
   // credential: applicationDefault()
 })
 
