f970c010322985ba13ec1d1102130ab47e440458
Joel Henry <joel.henry.023@gmail.com>
2022-03-15 23:26:15 -0500

    Initial Deploy

--- server/utils/firebase.config.js
+++ server/utils/firebase.config.js
@@ -1,6 +1,9 @@
-var firebase = require('firebase-admin')
-const serviceAccount = require('./config.json')
+require('dotenv').config()
 
+
+var firebase = require('firebase-admin')
+// const serviceAccount = require('./config.json')
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG)
 
 if (firebase.apps.length === 0) {
     firebase.initializeApp({
