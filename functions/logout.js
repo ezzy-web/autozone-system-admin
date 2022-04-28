3c7143d54251f15a520659df100f182f278526f1
Joel Henry <joel.henry.023@gmail.com>
2022-04-27 21:09:37 -0500

    Update Initials

--- functions/logout.js
+++
@@ -1,9 +0,0 @@
-
-const response = require('./utils/formattedResponse')
-const { logout } = require("./utils/firebase/firebaseAuth")
-
-exports.handler = async (event, context) => {
-    const { uid } = JSON.parse(event.body)
-    await logout(uid)
-    return response(200, "OK")
-}
